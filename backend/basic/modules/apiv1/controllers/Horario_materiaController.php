bbe58f9177baa52d64516306c2ed7212346a5ece
root <root@DESKTOP-4PG99KU.localdomain>
2023-04-27 13:05:09 -0300

    punto 14

---
+++ backend/basic/modules/apiv1/controllers/Horario_materiaController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\modules\apiv1\controllers;
+
+use yii\rest\ActiveController;
+
+/**
+ * Default controller for the `apiv1` module
+ */
+class Horario_materiaController extends ActiveController
+{
+    public $modelClass='app\models\HorarioMateria';
+}
